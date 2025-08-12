587d031f3f0048107476daca533a21dc85008ee8
Esmanur Eral <esmanur2eral@gmail.com>
2025-08-12 18:48:26 +0300

    Update ApiService

--- app/src/main/java/com/esmanureral/artframe/ApiService.kt
+++ app/src/main/java/com/esmanureral/artframe/ApiService.kt
@@ -2,6 +2,7 @@ package com.esmanureral.artframe
 
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
@@ -11,4 +12,9 @@ interface ApiService {
         @Query("limit") limit: Int = 10,
         @Query("fields") fields: String = "id,title,artist_display,image_id"
     ): Response<ArtworkListResponse>
+
+    @GET("artworks/{id}")
+    suspend fun getArtworkDetail(
+        @Path("id") id: Int
+    ): Response<ArtworkDetailResponse>
 }
