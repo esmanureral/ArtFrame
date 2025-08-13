2467ea00b3e5a6759d2867e4ef4afdc9c760e2a0
Esmanur Eral <esmanur2eral@gmail.com>
2025-08-13 21:12:40 +0300

    Add ArtistListFragment

---
+++ app/src/main/java/com/esmanureral/artframe/ArtistListFragment.kt
@@ -0,0 +1,69 @@
+package com.esmanureral.artframe
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.esmanureral.artframe.databinding.FragmentArtistListBinding
+
+
+class ArtistListFragment : Fragment() {
+    private var _binding: FragmentArtistListBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: ArtWorkViewModel by viewModels()
+    private lateinit var adapter: ArtistListAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentArtistListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setupRecyclerView()
+        observeViewModel()
+        viewModel.fetchArtists()
+    }
+
+    private fun setupRecyclerView() {
+        adapter = ArtistListAdapter()
+        with(binding) {
+            rvArtists.adapter = adapter
+            rvArtists.layoutManager = LinearLayoutManager(requireContext())
+
+            rvArtists.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrolled(rv: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(rv, dx, dy)
+                    checkForPagination(rv)
+
+                }
+            })
+        }
+    }
+
+    private fun checkForPagination(rv: RecyclerView) {
+        val layoutManager = rv.layoutManager as LinearLayoutManager
+        val totalItemCount = layoutManager.itemCount
+        val lastVisible = layoutManager.findLastVisibleItemPosition()
+        if (lastVisible >= totalItemCount - 2) {
+            viewModel.fetchArtists()
+        }
+    }
+
+    private fun observeViewModel() {
+        viewModel.artists.observe(viewLifecycleOwner) { newItems ->
+            newItems?.let { adapter.addData(it) }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
