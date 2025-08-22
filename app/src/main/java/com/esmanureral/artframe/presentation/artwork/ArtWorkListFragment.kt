9d8312d049bc17fa14dbc91e276624c81f1199bd
Esmanur Eral <esmanur2eral@gmail.com>
2025-08-22 23:19:42 +0300

    Update ArtWorkListFragment.kt

--- app/src/main/java/com/esmanureral/artframe/presentation/artwork/ArtWorkListFragment.kt
+++ app/src/main/java/com/esmanureral/artframe/presentation/artwork/ArtWorkListFragment.kt
@@ -8,7 +8,6 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.esmanureral.artframe.R
 import com.esmanureral.artframe.databinding.FragmentArtworkListBinding
@@ -34,26 +33,25 @@ class ArtworkListFragment : Fragment() {
         observeViewModel()
     }
 
-    private fun setupRecyclerView() = with(binding) {
+    private fun setupRecyclerView() {
         adapter = ArtworkAdapter { artwork ->
             navigateToDetail(artwork.id)
         }
-        recyclerView.layoutManager = GridLayoutManager(requireContext(), 3)
-        recyclerView.adapter = adapter
-        addPaginationListener(recyclerView)
-    }
 
-    private fun addPaginationListener(rv: RecyclerView) {
-        rv.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                super.onScrolled(recyclerView, dx, dy)
-                if (dy > 0) checkForPagination(recyclerView)
-            }
-        })
+        with(binding) {
+            recyclerView.layoutManager = GridLayoutManager(requireContext(), 3)
+            recyclerView.adapter = adapter
+            recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrolled(rv: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(rv, dx, dy)
+                    checkForPagination(rv)
+                }
+            })
+        }
     }
 
     private fun checkForPagination(rv: RecyclerView) {
-        val layoutManager = rv.layoutManager as LinearLayoutManager
+        val layoutManager = rv.layoutManager as GridLayoutManager
         val totalItemCount = layoutManager.itemCount
         val lastVisible = layoutManager.findLastVisibleItemPosition()
         if (lastVisible >= totalItemCount - 2) {
@@ -70,13 +68,8 @@ class ArtworkListFragment : Fragment() {
 
     private fun observeViewModel() {
         viewModel.artworks.observe(viewLifecycleOwner) { newItems ->
-            val currentList = adapter.currentList.toMutableList()
-            currentList.addAll(newItems)
-            adapter.submitList(currentList)
+            adapter.submitList(newItems.toList())
 
-            if (newItems.size < 10) {
-                viewModel.fetchArtworks()
-            }
         }
     }
 
