32bd5b88139c2c6f632c9eb2ed673cb4f7142b9d
esmanur <esmanur2eral@gmail.com>
2026-02-24 20:21:05 +0300

    chore: bump versionCode to 3 and versionName to 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.esmanureral.artframe"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
